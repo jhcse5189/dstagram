849fc8049e1e5ab9e53c6b3e0915e7e64724f68b
jhcse5189 <jhcse5189@gmail.com>
2019-09-15 03:55:42 +0900

    dstagram 1.0.7, - register Photo model to admin page - define MEDIA root for collecting static files from apps - upload first photo by admin page

--- config/settings.py
+++ config/settings.py
@@ -119,3 +119,7 @@ TIME_ZONE = 'Asia/Seoul'
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
